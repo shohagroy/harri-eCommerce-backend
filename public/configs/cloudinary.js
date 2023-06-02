d2ec1a187eff04ef109ee8abf456523723547f6e
shohag <pkshohag240@gmail.com>
2023-06-02 21:10:27 +0600

    public folder delete

--- public/configs/cloudinary.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.config = void 0;
-require("dotenv").config();
-exports.config = {
-    cloud_name: process.env.CLOUDINARY_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDINARY_API_SECRETE,
-};
