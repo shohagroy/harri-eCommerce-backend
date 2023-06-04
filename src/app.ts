6ffd1e6c7b6ea78e0eea04646937facb94e11c82
shohag <pkshohag240@gmail.com>
2023-06-04 16:13:54 +0600

    cors origin code update

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(
   cors({
     // origin: ["http://localhost:3000"],
-    // origin: "*",
+    origin: "*",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     preflightContinue: false,
     optionsSuccessStatus: 204,
